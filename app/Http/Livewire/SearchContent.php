fd998151bdcb89e5be4ef2b638cdeb90ec4cd04d
JonnyKuo <ji394su311203106@gmail.com>
2020-10-16 17:23:06 +0800

    init commit

---
+++ app/Http/Livewire/SearchContent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SearchContent extends Component
+{
+    public function render()
+    {
+        return view('livewire.search-content');
+    }
+}
